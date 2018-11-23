c091188122d760c664389f4aa31095787dc7d91b
Victor Sepulveda <vsepulveda@gmail.com>
2018-11-22 22:35:50 -0300

    Primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,20 @@
+import Server from "./classes/server";
+import router from './routes/router';
+import bodyPaser from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+
+// configuracion del body-parser
+server.app.use(bodyPaser.urlencoded({ extended: true }));
+server.app.use(bodyPaser.json());
+
+// CORS
+server.app.use(cors({ origin: true, credentials: true }));
+
+// usa las rutas
+server.app.use('/', router);
+
+server.start(() => {
+    console.log(`Servidor corriendo en el puerto ${ server.port }`);
+});
